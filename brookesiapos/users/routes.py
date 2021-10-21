3f4e5c47e19c52f370dfc49a897d3e5eb257f7bf
Adan Mercado <adanmercado.dev@gmail.com>
2021-10-21 00:48:00 -0500

    Added basic auth to users endpoint

--- brookesiapos/users/routes.py
+++ brookesiapos/users/routes.py
@@ -2,10 +2,12 @@ from flask import Response, json
 
 from . import users_bp
 from brookesiapos.database import db_manager
+from brookesiapos.utils.auth import http_auth
 
 @users_bp.route('/api/users')
+@http_auth.login_required
 def list_users():
-    data = db_manager.select_all_from_task('users')
+    data = db_manager.select_all_from_table('users')
 
     if data:
         return Response(
@@ -20,6 +22,7 @@ def list_users():
 
 
 @users_bp.route('/api/users/<int:item_id>')
+@http_auth.login_required
 def list_user(item_id):
     data = db_manager.select_one_from_table('users', item_id)
 
