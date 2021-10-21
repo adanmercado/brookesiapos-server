ded514e166581eee439ad0d6224d1bbd6a1aa17a
Adan Mercado <adanmercado.dev@gmail.com>
2021-10-21 00:36:47 -0500

    Added function to get a user

--- brookesiapos/database/db_manager.py
+++ brookesiapos/database/db_manager.py
@@ -21,3 +21,26 @@ def select_all_from_table(table_name):
         if conn:
             cursor.close()
             conn.close()
+
+
+def select_one_from_table(table_name, item_id):
+    conn = connection.create_connection()
+
+    sql_query = f'SELECT * FROM {table_name} WHERE id = {item_id}'
+
+    try:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute(sql_query)
+
+        row = cursor.fetchone()
+        if row:
+            data = dict(row)
+            return data
+    except sqlite3.Error as e:
+        print(f'Error at select_one_from_table(table_name, item_id): {str(e)}')
+        return False
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
--- brookesiapos/users/routes.py
+++ brookesiapos/users/routes.py
@@ -17,3 +17,19 @@ def list_users():
         return Response(status=500)
     else:
         return Response(status=200)
+
+
+@users_bp.route('/api/users/<int:item_id>')
+def list_user(item_id):
+    data = db_manager.select_one_from_table('users', item_id)
+
+    if data:
+        return Response(
+            response=json.dumps({ 'data': data }),
+            status=200,
+            content_type='application/json'
+        )
+    elif data == False:
+        return Response(status=500)
+    else:
+        return Response(status=200)
